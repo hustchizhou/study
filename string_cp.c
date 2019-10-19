930954c42dcec0d7498a4f5bb2a9385f04698404
hustchizhou <chizhou@outlook.com>
2019-10-19 12:55:25 +0800

    first commit

---
+++ string_cp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	char s1[]="hello";
+	char *s2;
+	unsigned int len;
+	len =sizeof(s1);
+	s2=(char *) malloc(len);
+	printf("len=0x%x\n",len);
+	printf("s2=0x%x\n",s2);
+	
+	s2 +=len;
+
+	printf("s2=0x%x\n",s2);
+	while(len)
+	{
+		s2--;
+		len--;
+		*s2 = s1[len];
+		printf("s2[%d]=%c\n",len,*s2);
+	}
+	printf("s1=%s\n",s1);
+	printf("s2=0x%x\n",s2);
+	printf("s2=%s\n",s2);
+        return 0;
+}
