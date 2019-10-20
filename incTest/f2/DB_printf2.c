f65187a8db5529abf614bdc1063b6eef7be6cdfe
hustchizhou <chizhou@outlook.com>
2019-10-20 22:21:05 +0800

    add hello src and remove inctest

--- incTest/f2/DB_printf2.c
+++
@@ -1,6 +0,0 @@
-#include "f2.h"
-
-void DB_Printf2(void)
-{
-	printf("DB_Printf2 is running!\n");
-}
--- incTest/f2/f2.h
+++
@@ -1,8 +0,0 @@
-#ifndef _F2_H
-#define _F2_H
-
-#include <stdio.h>
-
-void DB_Printf2(void);
-
-#endif
