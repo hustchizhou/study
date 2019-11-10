bfa395c19b98a896ee80d0247cecaf35c003b25c
hustchizhou <chizhou@outlook.com>
2019-11-10 19:38:41 +0800

    struct pointer plus 1

--- struct_size.c
+++ struct_size.c
@@ -7,10 +7,10 @@ typedef struct A{
 
 void main(void)
 {
-	structA p[10];
-	//p= (structA *)0x0;
-	printf("a addr=%x\tb addr=%x\t c addr=%x\n",&(p->a),&(p->b),&(p->c));
-	printf("p[1] addr=%x\n",&p[1]);
-	printf("structA byte size =%d \n",sizeof(structA));
-	printf("hello world\n");
+	structA *p;
+	p= (structA *)0x0;
+	printf("a addr=%d\tb addr=%d\t c addr=%d\n",(int)&(p->a),(int)&(p->b),(int)&(p->c));
+	printf("p+1 addr=%d\n",++p);
+	printf("a addr=%d\tb addr=%d\t c addr=%d\n",(int)&(p->a),(int)&(p->b),(int)&(p->c));
+	printf("structA byte size =%d \n",(int)sizeof(structA));
 }
